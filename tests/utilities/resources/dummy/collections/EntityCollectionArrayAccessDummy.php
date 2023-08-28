2ade04922fd1b8ab0a757a92302b98d195825c6d
akira wakaba <wakabadou@gmail.com>
2023-08-29 05:39:32 +0900

    #9 アクセス方法にプロパティを追加

---
+++ tests/utilities/resources/dummy/collections/EntityCollectionArrayAccessDummy.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ *   _____          ____  ____  ____
+ *  |_   ___ _  ___|  _ \|  _ \|  _ \
+ *    | |/ _` |/ __| | | | | | | | | |
+ *    | | (_| | (__| |_| | |_| | |_| |
+ *    |_|\__,_|\___|____/|____/|____/
+ *
+ * @category    TacDDD
+ * @package     TacDDD
+ * @author      wakaba <wakabadou@gmail.com>
+ * @copyright   Copyright (c) @2023  Wakabadou (http://www.wakabadou.net/) / Project ICKX (https://ickx.jp/). All rights reserved.
+ * @license     http://opensource.org/licenses/MIT The MIT License.
+ *              This software is released under the MIT License.
+ * @varsion     1.0.0
+ */
+
+declare(strict_types=1);
+
+namespace tacddd\tests\utilities\resources\dummy\collections;
+
+use tacddd\collections\entities\traits\EntityCollectionInterface;
+use tacddd\collections\entities\traits\EntityCollectionTrait;
+use tacddd\tests\utilities\resources\dummy\entities\EntityCollectionEntityDummy;
+
+final class EntityCollectionArrayAccessDummy implements EntityCollectionInterface
+{
+    use EntityCollectionTrait;
+
+    public static function getAllowedClass(): string
+    {
+        return EntityCollectionEntityDummy::class;
+    }
+
+    public static function createUniqueId(object $element): string|int
+    {
+        return $element->getId()->value;
+    }
+
+    public static function adjustKey(mixed $key, ?string $access_key = null): string|int
+    {
+        return \is_object($key) ? $key->value : $key;
+    }
+}
